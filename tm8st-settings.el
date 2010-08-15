9863b3d058358a433377864655120028086bba1d
tm8st <tm8st@hotmail.co.jp>
2010-08-15 19:42:44 +0900

    update.

--- tm8st-settings.el
+++ tm8st-settings.el
@@ -24,8 +24,10 @@
   )
 
   (global library
-      (tar-ball ftp://ftp.gnu.org/pub/gnu/global/global-5.9.1.tar.gz)
-      "this is source code tags util. for c, c++, java, etc")
+	  (tar-ball ftp://ftp.gnu.org/pub/gnu/global/global-5.9.1.tar.gz)
+	  "this is source code tags util. for c, c++, java, etc"
+	  nil
+	  ("cd global-5.9.1 && ./configure && make"))
  
  ;; (tm8st-anything-settings
  ;;  library
